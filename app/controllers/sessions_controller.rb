cb03f9ffc6a0a7eac07ac699037c2ecef8f467c4
Kyle McDonald <Kyle2mcdonald@gmail.com>
2017-05-18 16:38:48 -0400

    finished sessions routes and views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+get '/' do
+ "redirect to sessions"
+end
+
+
+get '/sessions/login' do
+  erb :'/sessions/login'
+end
+
+post '/sessions/login' do
+  @user = User.find_by(email: params[:user][:email])
+  if @user && @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    redirect '/'
+  else
+    @errors = ["Email and Password do not match our records."]
+    erb :'users/login'
+  end
+end
+
+get '/sessions/logout' do
+  session.clear
+  redirect '/'
+end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,22 +14,6 @@ post "/users" do
   end
 end
 
-get '/users/login' do
-  "login form"
-end
-
-post '/users/login' do
-  @user = User.find_by(email: params[:user][:email])
-  if @user && @user.authenticate(params[:user][:password])
-    session[:user_id] = @user.id
-    redirect '/users'
-  else
-    @errors = ["Email and Password do not match our records."]
-    erb :'users/login'
-  end
-end
-
-
 get "/users" do
   "user profile page"
 end
