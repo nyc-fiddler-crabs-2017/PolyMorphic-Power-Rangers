e0f41272d77468e9c6cfd25e42b2a778197cdede
Alex Persaud & Carlo Francisco & Carlos Grijalva & Kyle McDonald <pair+alexpersaud17.cfrancisco726.cgrijalva90.jkmcdonald@devbootcamp.com>
2017-05-19 15:08:02 -0400

    Fix CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,35 +5,35 @@ end
 
 # Create Posts
 20.times do
-  Post.create(title: Faker::ChuckNorris.fact + "?", question: Faker::Lorem.sentence, creator_id: rand(1..20))
+  Post.create(title: Faker::ChuckNorris.fact.chop + "?", question: Faker::Lorem.sentence, creator_id: rand(1..20))
 end
 
 # Create Answers
-20.times do
+200.times do
   Answer.create(best_answer: Faker::Boolean.boolean, body: Faker::Lorem.sentence, post_id: rand(1..20), answerer_id: rand(1..20))
 end
 
 # Create Comments on Posts
-10.times do
-  Comment.create(body: Faker::Hacker.say_something_smart, commentable_id: rand(1..15) , commentable_type: "Post", commenter_id: rand(1..20))
+100.times do
+  Comment.create(body: Faker::Hacker.say_something_smart, commentable_id: rand(1..20) , commentable_type: "Post", commenter_id: rand(1..20))
 end
 
 # Create Comments on Answers
 10.times do
-  Comment.create(body: Faker::Hacker.say_something_smart, commentable_id: rand(1..15) , commentable_type: "Answer", commenter_id: rand(1..20))
+  Comment.create(body: Faker::Hacker.say_something_smart, commentable_id: rand(1..20) , commentable_type: "Answer", commenter_id: rand(1..20))
 end
 
 # Create Votes on Answers
 50.times do
-  Vote.create(upvote: Faker::Boolean.boolean, votable_id: rand(1..15), votable_type: "Answer", user_id: rand(1..20))
+  Vote.create(upvote: Faker::Boolean.boolean, votable_id: rand(1..20), votable_type: "Answer", user_id: rand(1..20))
 end
 
 # Create Votes on Posts
 1000.times do
-  Vote.create(upvote: Faker::Boolean.boolean, votable_id: rand(1..15), votable_type: "Post", user_id: rand(1..20))
+  Vote.create(upvote: Faker::Boolean.boolean, votable_id: rand(1..20), votable_type: "Post", user_id: rand(1..20))
 end
 
 # Create Votes on Comments
 50.times do
-  Vote.create(upvote: Faker::Boolean.boolean, votable_id: rand(1..15), votable_type: "Comment", user_id: rand(1..20))
+  Vote.create(upvote: Faker::Boolean.boolean, votable_id: rand(1..10), votable_type: "Comment", user_id: rand(1..20))
 end
