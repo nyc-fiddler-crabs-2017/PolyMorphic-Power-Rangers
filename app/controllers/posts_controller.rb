14d587f17f38f7e6cc9811e71612284808c2db2d
Carlos Grijalva <grijalva.carlos@gmail.com>
2017-05-19 15:00:49 -0400

    Remove ensure login access in post/id request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,8 +27,6 @@ post '/posts' do
 end
 
 get '/posts/:id' do
-  # binding.pry
-  ensure_login_access
   @posts = find_and_ensure_post(params[:id])
   @current_user = current_user
   erb :'/posts/show'
