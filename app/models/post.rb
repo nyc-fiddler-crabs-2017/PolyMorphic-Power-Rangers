cbb739898405463b9a2e24598a8fff954c0a4338
Alex Persaud & Carlo Francisco & Carlos Grijalva & Kyle McDonald <pair+alexpersaud17.cfrancisco726.cgrijalva90.jkmcdonald@devbootcamp.com>
2017-05-19 14:48:09 -0400

    FINALLY fix associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :votes, as: :votable
-  belongs_to :creator, class_name: :User
+  belongs_to :creator, class_name: "User"
   has_many :answers
   has_many :comments, as: :commentable
 
--- db/migrate/20170518120523_create_posts.rb
+++ db/migrate/20170518120523_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.text :question, :null => false
-      t.references :user
+      t.references :creator
 
       t.timestamps(null: false)
     end
