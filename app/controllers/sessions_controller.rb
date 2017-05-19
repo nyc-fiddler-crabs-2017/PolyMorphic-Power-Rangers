4dc51d4dbc58f04b32b3b3310ea3df653300dbbd
Carlos Grijalva <grijalva.carlos@gmail.com>
2017-05-19 14:33:55 -0400

    Add stuff

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ end
 
 post '/sessions/login' do
   @user = User.find_by(username: params[:user][:username])
-  # binding.pry
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
     redirect '/'
