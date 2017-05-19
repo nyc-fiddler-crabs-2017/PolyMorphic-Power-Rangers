89c9f43ca851f7a643af5a7e8bfe91bc16cb45b0
Alex Persaud & Carlo Francisco & Carlos Grijalva & Kyle McDonald <pair+alexpersaud17.cfrancisco726.cgrijalva90.jkmcdonald@devbootcamp.com>
2017-05-19 10:25:52 -0400

    Fix authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ end
 
 post '/sessions/login' do
   @user = User.find_by(username: params[:user][:username])
-  binding.pry
+  # binding.pry
   if @user && @user.authenticate(params[:user][:password])
     session[:user_id] = @user.id
     redirect '/'
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,9 +4,8 @@ end
 
 post "/users" do
   @user = User.new(params[:user])
-
-  if @user.save?
-    @channels = @user.channels
+  # binding.pry
+  if @user.save
     session[:user_id] = @user.id
     redirect '/users'
   else
