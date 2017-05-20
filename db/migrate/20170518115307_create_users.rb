c2c9a291a5f18612d22f48b994012f41c221a2b3
Alex Persaud <alex_persaud01@yahoo.com>
2017-05-20 15:41:23 -0400

    Change layout and voting algorithm

--- db/migrate/20170518115307_create_users.rb
+++ db/migrate/20170518115307_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :username
-      t.string :email
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :username, null: false
+      t.string :email, null: false
       t.string :password_digest
 
       t.timestamps(null: false)
