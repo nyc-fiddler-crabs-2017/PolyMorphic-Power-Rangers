bc075e135d98a63efdb93cff928b0b56e5462de0
Carlos Grijalva <grijalva.carlos@gmail.com>
2017-05-20 18:02:57 -0400

    Add login.js for ajax

---
+++ public/js/login.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".container").on("click", ".login-button", function(event){
+      event.preventDefault();
+      var button = this;
+      debugger
+    });
+  // This is called after the document has loaded in its entirety
+  // This guarantees that any elements we bind to will exist on the page
+  // when we try to bind to them
+
+  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+});
